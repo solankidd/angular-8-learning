8caaa173363dfb638472e0bd47ab6e7a7ae6cc93
Dhaval Solanki <dhaval.d.solanki@gmail.com>
2019-09-25 12:15:50 +0530

    Udemy Course

---
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe 1',
+      'This is test desc',
+      'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg'
+    ),
+    new Recipe(
+      'Test Recipe 2',
+      'This is test desc',
+      'https://www.gimmesomeoven.com/wp-content/uploads/2014/03/Cajun-Jambalaya-Recipe-with-Andouille-Sausage-Shrimp-and-Chicken-3-1.jpg'
+    )
+  ];
+  constructor() {}
+
+  ngOnInit() {}
+}
