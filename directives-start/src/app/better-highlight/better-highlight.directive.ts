8420cace199ecd5118fc09630263fce5127ca878
Dhaval Solanki <dhaval.d.solanki@gmail.com>
2019-10-18 17:37:59 +0530

    Section 7: Directive deep dive (92, 93) directive in better way

---
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]'
+})
+export class BetterHighlightDirective implements OnInit {
+  constructor(private eleRef: ElementRef, private renderer: Renderer2) {}
+
+  ngOnInit() {
+    this.renderer.setStyle(this.eleRef.nativeElement, 'background-color', 'blue');
+  }
+}
