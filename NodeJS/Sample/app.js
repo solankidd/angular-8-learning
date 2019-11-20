96554d7575086dfe1cb72a699e321cf728850f1d
Dhaval Solanki <dhaval.d.solanki@gmail.com>
2019-11-20 17:29:32 +0530

    Node event understading

--- NodeJS/Sample/app.js
+++ NodeJS/Sample/app.js
@@ -1,7 +1,46 @@
 const http = require('http');
+const fs = require('fs');
+
 const server = http.createServer((req, res) => {
-  console.log(req);
-  process.exit();
+  const url = req.url;
+  const method = req.method;
+  if(url === '/'){
+    res.write('<html>');
+    res.write('<head><title>Enter message</title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+    res.write('</html>');
+    return res.end();
+  }
+
+  if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/')
+      return res.end(); // if we don't return, it will throw error
+      // why?
+      // because code after this if ends will execute first
+      // than this will execute so it will try to give response again
+      // this will throw error
+      // if we return, it will not execute code after if
+    })
+
+  }
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>Other message</title></head>');
+  res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+  res.write('</html>');
+  res.end();
+//  process.exit();
 });
 
 server.listen(3000);
